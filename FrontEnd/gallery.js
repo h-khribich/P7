a30972046c3652365271760636450aae16f95caf
h-khribich <hamza.khribich@gmail.com>
2024-11-04 21:07:34 +0100

    Added filter feature

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -13,19 +13,28 @@ const fetchGetData = async (endpoint) => {
 }
 
 // FILL GALLERY
-const fillGallery = async () => {
+const fillGallery = async (param = 0) => {
   const works = await fetchGetData("works");
+
+  const fill = (arr) => {
+    gallery.innerHTML = ''
+    
+    arr.map((work) => {
+      const item = 
+      `
+      <figure data-category-id="${work.categoryId}">
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+      </figure>
+      `
+      gallery.innerHTML += item
+    })
+  }
   
-  works.map((work) => {
-    const item = 
-    `
-    <figure>
-			<img src="${work.imageUrl}" alt="${work.title}">
-			<figcaption>${work.title}</figcaption>
-		</figure>
-    `
-    gallery.innerHTML += item
-  })
+  // FILL BY CATEGORY ID, 0 IS THE DEFAULT DATASET TO FETCH ALL
+  const filteredWorks = works.filter((work) => work.categoryId === param)
+
+  param === 0 ? fill(works) : fill(filteredWorks)
 }
 
 // FILL CATEGORIES
@@ -33,11 +42,29 @@ const fillCategories = async () => {
   const categories = await fetchGetData("categories");
 
   categories.map((category) => {
-    const item = `<button class="btn">${category.name}</button>`
+    const item = `<button type="button" data-category-id="${category.id}" class="btn-categories">${category.name}</button>`
     categoryWrapper.innerHTML += item;
+  })
+
+  const categoryBtns = Array.from(document.querySelectorAll(".categories > .btn-categories"));
+
+  // FILTER EVENT
+  categoryBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      // TOGGLE 'SELECTED' STATUS
+      const previousSelected = document.querySelector(".categories > .selected")
+      previousSelected.classList.remove("selected")
+
+      e.target.classList.add("selected")
+      const id = Number(e.target.dataset.categoryId)
+      
+      fillGallery(id)
+    })
   })
 }
 
-
+// INITIALIZATION
 fillGallery()
 fillCategories()
